3f4758e2760f3a3df0037ce190020a67cc9f8c79
Rain Saavel <rain.saavel@mindworks.ee>
2015-06-15 11:49:18 +0300

    tinymixtapes

---
+++ js/tinymixtapes.js
@@ -0,0 +1,32 @@
+$(window).load(function() {
+	console.log("hello there");
+	
+
+	
+	$("#music-reviews .item,  .music-reviews .item").each(function(i){
+		var albumgroup = [];
+		var m = $(this);
+		
+		var album = encodeURIComponent($(m).find(".item-subtitle").text().replace(':', ''));
+		var artist = encodeURIComponent($(m).find(".item-title").text().replace(':', ''));
+		var btnbox = "adfi4dasdasd"+i;
+		$(m).addClass(btnbox);
+		albumgroup[0]=artist;
+		albumgroup[1]=album;
+		albumgroup[2]="."+btnbox;
+		albumslist.push(albumgroup);
+	}).promise().done( function(){ 
+		countertotal = albumslist.length;
+		
+		getData();
+		console.table(albumslist);
+
+	 } );
+
+
+	
+
+
+
+});	
+
