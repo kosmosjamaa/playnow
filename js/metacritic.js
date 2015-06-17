6ed6c6bd9f332c96c03bfffd0076a70812a0f417
Rain Saavel <rainsaavel@Rains-MBP.lan>
2015-06-17 23:27:38 +0300

    metacritic

---
+++ js/metacritic.js
@@ -0,0 +1,41 @@
+$(window).load(function() {
+	console.log("hello there");
+	
+
+	
+	$(".product, .product_row").each(function(i){
+		var albumgroup = [];
+		var m = $(this);
+
+		var album = encodeURIComponent($.trim($(m).find(".product_title a").text()).replace(':', ''));
+		var artistobj =  $(m).find(".product_artist").clone();
+		artistobj.find(".label").remove();
+		artistobjt=$.trim(artistobj.text()).replace(':', '');
+		if(m.parent().hasClass('list_product_summaries')){
+			artistobjt = artistobjt.slice(2);
+		}
+		
+		
+		var artist = encodeURIComponent(artistobjt);
+		
+		var btnbox = "adfi4dasdasd"+i;
+		$(m).find("div.product_title").addClass(btnbox);
+		albumgroup[0]=artist;
+		albumgroup[1]=album;
+		albumgroup[2]="."+btnbox;
+		albumslist.push(albumgroup);
+	}).promise().done( function(){ 
+		countertotal = albumslist.length;
+		
+		getData();
+		console.table(albumslist);
+
+	 } );
+
+
+	
+
+
+
+});	
+
