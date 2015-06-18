f56cf076785397c7853b6ed46532b9dc1627816a
Rain Saavel <rain.saavel@mindworks.ee>
2015-06-18 11:35:21 +0300

    readme

---
+++ js/mimaroglumusic.js
@@ -0,0 +1,33 @@
+$(window).load(function() {
+	console.log("hello there");
+	
+
+	
+	$(".wsite-com-category-product").each(function(i){
+		var albumgroup = [];
+		var m = $(this);
+		var releasedata = $.trim($(this).find(".wsite-com-category-product-name").text());
+		var album = encodeURIComponent(releasedata.substr(releasedata.indexOf(" - ") + 3).replace(':', ''));
+		var artist = encodeURIComponent(releasedata.substring(0,releasedata.indexOf(" - ")).replace(':', ''));		
+		
+		var btnbox = "adfi4dasdasd"+i;
+		$(m).find(".wsite-com-category-product-wrap").addClass(btnbox);
+		albumgroup[0]=artist;
+		albumgroup[1]=album;
+		albumgroup[2]="."+btnbox;
+		albumslist.push(albumgroup);
+	}).promise().done( function(){ 
+		countertotal = albumslist.length;
+		
+		getData();
+		console.table(albumslist);
+
+	 } );
+
+
+	
+
+
+
+});	
+
--- readme.md
+++ readme.md
@@ -1,1 +1,10 @@
--------------
+
+How does it work? Well, it's a browser (Chrome) plugin  indexes albums on the page and then injects spotify link if there is any. <br>
+
+At the moment "works" with:
+
+<ul>
+	<li>Recordstores: Boomkat, Juno, Mimaroglu, Aquarius</li>
+	<li>Webzines: Tinymixtapes, The Quietus, Drowned In Sound</li>
+	<li>Databases: Discogs, Metacritic, Metal Archives, Rate Your Music</li>
+</ul>	
