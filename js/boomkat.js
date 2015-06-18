5fd1dc42bfa889efa6aa9df4c65f1d0861adb1b5
Rain Saavel <rain.saavel@mindworks.ee>
2015-06-18 10:36:53 +0300

    boomkat line

--- js/boomkat.js
+++ js/boomkat.js
@@ -8,6 +8,12 @@ $(window).load(function() {
 		var album = encodeURIComponent($(m).find(".title").text().replace(':', ''));
 		var artist = encodeURIComponent($(m).find(".artist").text().replace(':', ''));
 		var btnbox = "adfi4dasdasd"+i;
+		if( $(this).parent().hasClass("line")){
+			var m = $(this).closest(".line");
+			var artist = encodeURIComponent($(m).find(".meta h4").text().replace(':', ''));
+			var album = encodeURIComponent($(m).find(".meta h4").next("p").text().replace(':', ''));
+		}
+		
 		$(m).addClass(btnbox);
 		albumgroup[0]=artist;
 		albumgroup[1]=album;
